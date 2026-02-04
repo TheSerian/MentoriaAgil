c4156c31e54e3d90581e06765805bb0e936956a5
ThaysonScript <thaysonguedes01@gmail.com>
2026-02-04 12:44:48 -0300

    init: projeto spring boot base criado com dependencias para projetos web, ferramentas de restart, reloading, validacao e banco de dados postgresql

---
+++ backend/src/test/java/com/mentoria/agil/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mentoria.agil.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
