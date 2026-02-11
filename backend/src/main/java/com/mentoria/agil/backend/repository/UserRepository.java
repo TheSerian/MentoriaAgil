93269a58afcb42ec93f86c8c180ec7150fdcff9d
Joneilson César Botelho Júnior <joneilsonjunior@gmail.com>
2026-02-11 02:56:54 -0300

    feat: autenticação de usuário e gestão de roles com config de segurança

---
+++ backend/src/main/java/com/mentoria/agil/backend/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.mentoria.agil.backend.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mentoria.agil.backend.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
